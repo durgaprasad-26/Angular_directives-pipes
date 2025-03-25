fa2bc5e90912e39a432a4fa8ca7538bc00762238
durga prasad <rprasad2614@gmail.com>
2025-03-24 22:59:56 -0700

    these are direcitves and pipes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directive_and_pipes';
+  name='durgaprasad';
+  check:boolean=true;
+  islogged=0;
+}
---
+++ src/app/custom-pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customPipe'
+})
+export class CustomPipePipe implements PipeTransform {
+
+  transform(value:String):String {
+    return value.split('').reverse().join('');;
+  }
+
+}
